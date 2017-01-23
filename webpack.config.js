62d3300a7e6e674a17415a854487ae065ff8fd68
Loris Guerra <loris4526@gmail.com>
2017-01-24 00:31:00 +0100

    Create Pages and Add Routes & Links

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,13 @@ var config = {
   },
   resolve: {
     root: __dirname,
-    alias: {},
+    alias: {
+      Main: 'app/components/Main.jsx',
+      Nav: 'app/components/Nav.jsx',
+      Weather: 'app/components/Weather.jsx',
+      About: 'app/components/About.jsx',
+      Examples: 'app/components/Examples.jsx'
+    },
     extensions: ['','.js','.jsx']
   },
   module: {
